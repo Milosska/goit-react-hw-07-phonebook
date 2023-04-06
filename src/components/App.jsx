4487b1efe1c790da92f17b3855416dcb090199ae
Liudmyla Deinychenko <110258611+Milosska@users.noreply.github.com>
2023-04-06 23:56:23 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import { GlobalStyles } from './GlobalStyles';
+import 'modern-normalize';
+
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+export const App = () => {
+  return (
+    <div>
+      <GlobalStyles />
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </div>
+  );
+};
---
+++ src/components/ContactListElem/ContactListElem.styled.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const ListElem = styled.li`
+  width: 350px;
+  display: flex;
+  justify-content: space-between;
+
+  :not(:last-child) {
+    margin-bottom: 10px;
+  }
+`;
+
+export const Text = styled.p`
+  font-size: 18px;
+`;
+
+export const Button = styled.button`
+  display: inline-block;
+  margin: 0;
+  padding: 5px 10px;
+
+  border: 1px solid black;
+  background-color: transparent;
+  transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  :hover,
+  :focus {
+    background-color: #4369f5;
+    color: white;
+  }
+`;
